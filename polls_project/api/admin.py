2559cc30d028e82eba84eaeda23aab8345b33d75
karlygach <karygachkenenbaeva@gmail.com>
2021-12-15 15:06:50 +0600

    poll model is registered in admin

--- polls_project/api/admin.py
+++ polls_project/api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Poll
+
+
+@admin.register(Poll)
+class PollAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'date_start', 'date_end']
+    list_filter = ['date_start', 'date_end']
+    search_fields = ['name']
+    fields = ['id','name', 'date_start', 'date_end', 'description']
+    readonly_fields = ['id' ,]
