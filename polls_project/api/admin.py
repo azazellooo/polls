506b38d420c82fd67a9ff484b94b204e96d010fd
karlygach <karygachkenenbaeva@gmail.com>
2021-12-15 15:39:13 +0600

    question model is registered in admin

--- polls_project/api/admin.py
+++ polls_project/api/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Poll
+from .models import Poll, Question
 
 
 @admin.register(Poll)
@@ -10,3 +10,13 @@ class PollAdmin(admin.ModelAdmin):
     search_fields = ['name']
     fields = ['id','name', 'date_start', 'date_end', 'description']
     readonly_fields = ['id' ,]
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'poll', 'type', 'text']
+    list_filter = ['type', 'poll']
+    search_fields = ['text']
+    fields = ['id','text', 'poll', 'type']
+    readonly_fields = ['id' ,]
+
